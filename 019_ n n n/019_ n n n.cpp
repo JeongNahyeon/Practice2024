e8104bdd0e51e5eb804edf89601b563eb1e915f2
JeongNahyeon <skgus0031@gmail.com>
2024-09-12 11:20:44 +0900

    Add files via upload

---
+++ 019_ n n n/019_ n n n.cpp
@@ -0,0 +1,25 @@
+﻿// 019_ n n n.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("n을 입력: ");
+    scanf_s("%d", &n);
+
+    int i = 1;
+    int sumOdd = 0;     // 홀수합
+    int sumEven = 0;    // 짝수합
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0)
+            sumEven += i;
+        else
+            sumOdd += i;
+    }
+    printf("1부터 %d까지의 홀수의 합: %d\n", n, sumOdd);
+    printf("1부터 %d까지의 짝수의 합: %d\n", n, sumEven);
+}
---
+++ 022_7x1/022_7x1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+    int n;
+
+    // 출력할 단 수 입력받기
+    printf("출력할 단 수를 입력하세요: ");
+    scanf_s("%d", &n);
+
+    // 구구단 출력
+    for (int i = 1; i <= 9; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    
+}
